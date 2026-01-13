fcc6959ca1c82145d1c99df0ef9b7802f33c7d96
MartinPPY <martinsantiago.se@gmail.com>
2026-01-13 13:43:19 -0300

    agregando product-detail.html

---
+++ assets/js/products.js
@@ -0,0 +1,51 @@
+//SCRIPT PARA COLOCAR PRODUCTOS EN EL LA SECCION PRODUCTOS
+const renderProducts = () => {
+    const container = document.querySelector("#products > .row")
+
+    for (let i = 0; i <= 2; i++) {
+        container.innerHTML += `
+        <div class="col">
+            <div class="card bg-black text-white h-100 border-secondary">
+                <img src="${productos[i].image}" class="card-img-top" alt="Juego">            
+                <div class="card-body">
+                    <h5 class="card-title">${productos[i].nombre}</h5>
+                    <p class="card-text">
+                    Categoria: ${productos[i].categoria}
+                    </p>
+                </div>
+                <div class="card-footer d-flex flex-column gap-2 border-secondary">
+                    <div>
+                        <span class="fw-bold text-info">
+                            $ ${new Intl.NumberFormat('es-CL').format(productos[i].precio)} CLP
+                        </span>
+                    </div>
+                    <div class="d-flex gap-2 justify-content-between align-items-center">
+                        <button 
+                            class="btn btn-sm btn-outline-info"
+                            onclick="getProductDetail('${productos[i].codigo}')"
+                        >
+                            <i class="bi bi-eye"></i>
+                            Ver detalle
+                        </button>
+                        <button class="btn btn-sm btn-level-up text-white">
+                            <i class="bi bi-cart-plus"></i>
+                            Añadir al carrito
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+
+
+const getProductDetail = (id) => {
+    window.location.href = "product-detail.html?id="+id
+}
+
+
+//LISTENER PARA CARGAR LAS FUNCIONES EN TODA LA PAGINA
+addEventListener("load", () => {
+    renderProducts()
+})
