c50aed90c6353bada7ae7351ee38954c537f02c7
MartinPPY <martinsantiago.se@gmail.com>
2026-01-14 13:42:24 -0300

    funcionalidad del carrito casi lista

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,4 +1,4 @@
-const addToCart = (codigo) => {
+function addToCart(codigo) {
     const producto = productos.find(producto => producto.codigo === codigo);
     const cartItem = carrito.find(item => item.codigo === codigo);
 
@@ -7,19 +7,53 @@ const addToCart = (codigo) => {
         producto.stock--;
         carrito.push(producto);
         localStorage.setItem('carrito', JSON.stringify(carrito));
-        traerResumen();
+        getProductsOfCart()
     } else {
         cartItem.cantidad ? cartItem.cantidad++ : cartItem.cantidad = 1;
         producto.stock--;
         localStorage.setItem('carrito', JSON.stringify(carrito));
-
-        traerResumen();
+        getProductsOfCart()
     }
 }
 
-const getProductsOfCart = () => {
+function delToCart(codigo) {
+    const cartItem = carrito.find(item => item.codigo === codigo);
+    if (cartItem) {
+        cartItem.cantidad--;
+        if (cartItem.cantidad === 0) {
+            carrito = carrito.filter(item => item.codigo !== codigo);
+        }
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        getProductsOfCart()
+    }
+}
+
+function delAllFromCart() {
+    Swal.fire({
+        title: "¿Estás seguro?",
+        text: "No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, Vaciar carrito"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem('carrito');
+            getProductsOfCart()
+            Swal.fire({
+                text: "El carrito ha sido vaciado.",
+                icon: "success"
+            });
+        }
+    });
+
+}
+
+function getProductsOfCart() {
     const cart = JSON.parse(localStorage.getItem('carrito')) || [];
     const container = document.querySelector("#cart-items");
+    container.innerHTML = "";
     for (let item of cart) {
         container.innerHTML += `
         <div class="card mb-3 cart-item bg-dark text-white border">
@@ -36,7 +70,7 @@ const getProductsOfCart = () => {
                 </div>
                 <div class="col-md-4 text-center">
                     <div class="d-flex justify-content-center align-items-center">
-                        <button class="btn btn-outline-secondary btn-sm" onclick="">−</button>
+                        <button class="btn btn-outline-secondary btn-sm" onclick="delToCart('${item.codigo}')">−</button>
                             <span class="mx-3 cantidad">${item.cantidad}</span>
                         <button class="btn btn-outline-secondary btn-sm" onclick="addToCart('${item.codigo}')">+</button>
                     </div>
