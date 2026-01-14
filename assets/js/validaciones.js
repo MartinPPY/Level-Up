5a767995bf7db41d31c9b6c8cc8377c91d3ef9ee
IgnacioGodoyM <igna.godoym@duocuc.cl>
2026-01-14 15:08:00 -0300

    Validacion De Login Terminada

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -2,24 +2,25 @@
 const email = document.getElementById("email")
 const pass = document.getElementById("password")
 const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
+
 
 form.addEventListener("submit", e => {
     e.preventDefault()
 
     const errores = []
 
-
-    let warnings = ""
-
-    let entrar = false
-
+    
     let regexEmail = /^[a-zA-Z0-9._%+-]+@(gmail\.com|duoc\.cl|duocuc\.cl)$/
 
     if (!regexEmail.test(email.value)) {
         errores.push("Solo se permiten correos @gmail.com, @duoc.cl o @duocuc.cl")
-        entrar = true
+
     }
+
+    if (pass.value.length < 8) {
+        errores.push("La contraseña debe tener al menos 8 caracteres")
+    }
+
 
     if (errores.length > 0) {
         const errorDiv = document.querySelector("#errores");
@@ -34,12 +35,4 @@ form.addEventListener("submit", e => {
         return
     }
 
-    /*if (pass.value.length < 4 && pass.value.length > 10) {
-        warnings += `La contraseña no es valida <br>`
-        entrar = true
-    }
-
-    if (entrar) {
-        parrafo.innerHTML = warnings
-    }*/
 })
